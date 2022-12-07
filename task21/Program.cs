c45f99380a637c918c22320e94807835a6223cb0
Zhdana <zhdanasulina@yandex.ru>
2022-12-07 09:11:22 +0300

    second commit

---
+++ task21/Program.cs
@@ -0,0 +1,7 @@
+﻿double[] points = new double[6];
+for (int i=0; i<points.Length; i++)
+{
+    points[i]=Convert.ToDouble(Console.ReadLine());
+}    
+double ab = Math.Sqrt((points[0]-points[3])*(points[0]-points[3]) + (points[1]-points[4])*(points[1]-points[4]) + (points[2]-points[5])*(points[2]-points[5]));
+Console.WriteLine(ab);
